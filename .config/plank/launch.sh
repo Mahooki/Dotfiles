b8def0c1214334dec1ec3ab29e52f389f38884ab
Noah McClelland <noahmccle@gmail.com>
2020-10-13 23:14:51 -0400

    add bspwmrc; launch plank dock

---
+++ .config/plank/launch.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q plank
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x plank >/dev/null; do sleep 1; done
+
+# Launch dock1 and dock2
+plank -n dock1 &
+plank -n dock2 &
